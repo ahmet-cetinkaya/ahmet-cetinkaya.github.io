9f45e719455425e5d20af611dd56b8efab988adf
Ahmet Çetinkaya <ahmetcetinkaya7@outlook.com>
2022-09-02 20:01:13 +0300

    feat(*): add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -172,5 +172,32 @@ module.exports = {
       },
     },
     'gatsby-plugin-sitemap',
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          process.env.GOOGLE_ANALYTICS_TRACKING_ID, // Google Analytics
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        // gtagConfig: {
+        //   optimize_id: 'OPT_CONTAINER_ID',
+        //   anonymize_ip: true,
+        //   cookie_expires: 0,
+        // },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+          // Setting this parameter is also optional
+          // respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          // exclude: [],
+          // Defaults to https://www.googletagmanager.com
+          // origin: 'YOUR_SELF_HOSTED_ORIGIN',
+        },
+      },
+    },
   ],
 };

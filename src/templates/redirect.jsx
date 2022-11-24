b2d30f2c710a9daffff1e6d792fb73cc73f8e80e
Ahmet Çetinkaya <ahmetcetinkaya7@outlook.com>
2022-11-24 15:26:31 +0300

    refactor: use meta tag to redirects

--- src/templates/redirect.jsx
+++ src/templates/redirect.jsx
@@ -1,26 +1,25 @@
-import { graphql } from 'gatsby';
 import * as React from 'react';
-import { useEffect } from 'react';
+
 import Seo from '../core/components/Seo/Seo';
+import { graphql } from 'gatsby';
 
-function Redirect({ pageContext: { redirectTo } }) {
-  useEffect(() => {
-    window.location.href = redirectTo;
-  }, []);
-
-  return <div />;
+function Redirect() {
+  return <></>;
 }
 
-export function Head({ data }) {
+export function Head({ data, pageContext: { redirectTo } }) {
   // todo: use instead usei18next (https://github.com/microapps/gatsby-plugin-react-i18next/issues/150)
   const locales = JSON.parse(
     data.locales.edges.find((edge) => edge.node.ns === 'redirect').node.data
   );
   return (
-    <Seo
-      title={`${locales.redirecting}...`}
-      description={`${locales.redirecting}: ${data.redirectTo}`}
-    />
+    <>
+      <Seo
+        title={`${locales.redirecting}...`}
+        description={`${locales.redirecting}: ${data.redirectTo}`}
+      />
+      <meta http-equiv="refresh" content={`0;URL=${redirectTo}`} />
+    </>
   );
 }
 
